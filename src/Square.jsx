86998fad58c7a1374ae66fd465c0d3d2d820125f
rachelholmquist <Rachelflorek18@outlook.com>
2022-12-13 18:30:33 -0800

    first commit

---
+++ src/Square.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Square(props) {
+    return(
+        <div className="square">
+            {props.propVar}
+        </div>
+    );
+};
+
+export default Square;
+
