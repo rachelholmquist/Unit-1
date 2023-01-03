1e510246d34574269249453d6fb72a0f8222e5df
rachelholmquist <Rachelflorek18@outlook.com>
2023-01-02 20:02:37 -0800

    working to fix profile issue

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Square from "./Square"
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Square from "./Square";
 
 function App() {
-    const [squares, setSquares] = useState(["","","","","","","","",""])
-    const [player, setPlayer] = useState(true);
-    
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState(true);
+
+  const handleClick = () => {
+    setSquares(["", "", "", "", "", "", "", "", ""])
+    setPlayer(true)
+  }
+
   return (
-    
     <div className="App">
-      <Square 
-      squares = {squares}
-      setSquares = {setSquares}
-      player = {player}
-      setPlayer = {setPlayer}
-       />
+      <div className="container">
+        {squares.map((value, index) => {
+          return (
+            <Square
+              setSquares={setSquares}
+              index={index}
+              squareValue={value}
+              squares={squares}
+              player={player}
+              setPlayer={setPlayer}
+            />
+          );
+        })}
+      </div>
+      <button onClick={handleClick}>Reset</button>
     </div>
   );
 }
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 
 function Square(props) {
+    const handleClick = () => {
+        if(!props.squareValue) {
+            if (props.player) {
+                props.squares.splice(props.index, 1, "X");
+                props.setSquares(props.squares);
+                props.setPlayer(!props.player);
+            } else {
+                props.squares.splice(props.index, 1, "O");
+                props.setSquares(props.squares);
+                props.setPlayer(!props.player);
+            }
+        }
+    };
     return(
-            console.log(props.squares, props.player)
-        
+            <div className="square" onClick={handleClick}>
+                {props.squareValue === "O" ? <img src="https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png"/> : props.squareValue};
+            </div>
     );
+        
+    
 };
 
 export default Square;
